c01bf538da4319915862dfa2753d5c2564d097e4
bacsumu <bacsumu@gmail.com>
2023-09-25 18:21:57 +0900

    test

--- src/routes/MainRoute.tsx
+++ src/routes/MainRoute.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NoPage from "../pages/error/NoPage";
 import Blogs from "../pages/blogs/Blogs";
 import ContentsList from "../pages/blogs/ContentsList";
@@ -7,19 +7,22 @@ import Main from "../pages/main/Main";
 import MainLayout from "../pages/layouts/Main";
 import About from "../pages/about/Home";
 import Home from "../pages/home/Home";
+import React from "react";
 
 export default function MainRoute() {
   return (
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<Home />}></Route>
-        <Route path="blogs" element={<Blogs />}>
-          <Route index element={<ContentsList />}></Route>
-          <Route path="write" element={<ContentWrite />} />
+    <React.StrictMode>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />}></Route>
+          <Route path="blogs" element={<Blogs />}>
+            <Route index element={<ContentsList />}></Route>
+            <Route path="write" element={<ContentWrite />} />
+          </Route>
+          <Route path="about" element={<About />}></Route>
         </Route>
-        <Route path="about" element={<About />}></Route>
-      </Route>
-      <Route path="*" element={<NoPage />} />
-    </Routes>
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </React.StrictMode>
   );
 }
